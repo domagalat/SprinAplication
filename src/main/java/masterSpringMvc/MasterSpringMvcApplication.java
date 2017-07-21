b968752a859f895514aa70092750ad7ef11e55b0
domagalat <tomdom886@gmail.com>
2017-07-21 18:03:12 +0200

    Dodano pliki result.html i dafault.html i zmiany w TweetController.java

--- src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
+++ src/main/java/masterSpringMvc/MasterSpringMvcApplication.java
@@ -1,12 +1,27 @@
 package masterSpringMvc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigurationProperties
+@ComponentScan
 @SpringBootApplication
+@EntityScan
+
+
+
 public class MasterSpringMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MasterSpringMvcApplication.class, args);
+
 	}
 }
