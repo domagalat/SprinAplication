20264537ae5005fc2a44ec3fd614fcc5e85c58c9
domagalat <tomdom886@gmail.com>
2017-10-10 23:43:49 +0200

    nowe controlery, nie wyświetla tłumaczenia nadal11

---
+++ src/main/java/masterSpringMvc/config/PicturesUploadProperties.java
@@ -0,0 +1,30 @@
+package masterSpringMvc.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+
+/**
+ * Created by TomDom on 2017-10-10.
+ */
+@ConfigurationProperties(prefix = "upload.pictures")
+public class PicturesUploadProperties {
+    private Resource uploadPath;
+    private Resource anonymousPicture;
+
+    public Resource getAnonymousPicture() {
+        return anonymousPicture;
+    }
+
+    public void setAnonymousPicture(String anonymousPicture) {
+        this.anonymousPicture = new DefaultResourceLoader().getResource(anonymousPicture);
+    }
+
+    public Resource getUploadPath() {
+        return uploadPath;
+    }
+
+    public void setUploadPath(String uploadPath) {
+        this.uploadPath = new DefaultResourceLoader().getResource(uploadPath);
+    }
+}
